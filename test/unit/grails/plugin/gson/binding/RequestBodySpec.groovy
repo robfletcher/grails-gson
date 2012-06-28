f5c748041edc3c743d1ea43ce8b7a42b1b6b3aa0
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-28 17:20:19 +0100

    Start to map out how HTTP request deserialization & binding will work

--- test/unit/grails/plugin/gson/binding/RequestBodySpec.groovy
+++ test/unit/grails/plugin/gson/binding/RequestBodySpec.groovy
@@ -1,11 +1,53 @@
 package grails.plugin.gson.binding
 
-/**
- * Created with IntelliJ IDEA.
- * User: telly
- * Date: 28/06/2012
- * Time: 09:28
- * To change this template use File | Settings | File Templates.
- */
-class RequestBodySpec {
+import grails.persistence.Entity
+import grails.test.mixin.Mock
+import org.codehaus.groovy.grails.plugins.testing.GrailsMockHttpServletRequest
+import spock.lang.Specification
+import spock.util.mop.ConfineMetaClassChanges
+
+import javax.servlet.http.HttpServletRequest
+
+import com.google.gson.*
+
+@ConfineMetaClassChanges(HttpServletRequest)
+@Mock(Album)
+class RequestBodySpec extends Specification {
+
+    void setup() {
+        GrailsMockHttpServletRequest.metaClass.getJSON = { ->
+            new JsonParser().parse(delegate.reader)
+        }
+    }
+
+    void 'can get JSON data from request'() {
+        given:
+        def request = new GrailsMockHttpServletRequest()
+        request.content = '{"artist":"Metric","title":"Synthetica"}'.bytes
+
+        expect:
+        JsonElement.isAssignableFrom(request.JSON.getClass())
+        request.JSON.artist.getAsString() == 'Metric'
+        request.JSON.title.getAsString() == 'Synthetica'
+    }
+
+    void 'can bind request json direct to domain class'() {
+        given:
+        def request = new GrailsMockHttpServletRequest()
+        request.content = '{"artist":"Metric","title":"Synthetica"}'.bytes
+
+        when:
+        def album = new Album(request.JSON)
+
+        then:
+        album.artist == 'Metric'
+        album.title == 'Synthetica'
+    }
+
+}
+
+@Entity
+class Album {
+    String artist
+    String title
 }
