431dc4a94f38bcfbf28c3ea7e55055c5bcda0606
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-02-14 06:09:26 +0000

    failing test for #9

--- test/unit/grails/plugin/gson/binding/HttpResponseSpec.groovy
+++ test/unit/grails/plugin/gson/binding/HttpResponseSpec.groovy
@@ -1,12 +1,10 @@
 package grails.plugin.gson.binding
 
-import spock.lang.Specification
-import spock.util.mop.ConfineMetaClassChanges
-
 import javax.servlet.http.HttpServletResponse
-
 import grails.plugin.gson.*
 import grails.test.mixin.*
+import spock.lang.*
+import spock.util.mop.ConfineMetaClassChanges
 
 @ConfineMetaClassChanges(HttpServletResponse)
 @TestFor(AlbumController)
@@ -21,7 +19,7 @@ class HttpResponseSpec extends Specification {
         controller = new AlbumController()
     }
 
-    void 'can render using GSON converter'() {
+    void 'can render a domain instance list using GSON converter'() {
         given:
         def album = new Album(artist: 'David Bowie', title: 'The Rise and Fall of Ziggy Stardust and the Spiders From Mars').save(failOnError: true)
 
@@ -32,10 +30,37 @@ class HttpResponseSpec extends Specification {
         response.contentAsString == /[{"artist":"David Bowie","title":"The Rise and Fall of Ziggy Stardust and the Spiders From Mars","id":$album.id}]/
     }
 
+	@Issue('https://github.com/robfletcher/grails-gson/issues/9')
+    void 'can render a simple map using GSON converter'() {
+        when:
+        controller.error()
+
+        then:
+        response.contentAsString == /{"error":"o noes"}/
+    }
+
+    void 'can render a simple list using GSON converter'() {
+        when:
+        controller.errors()
+
+        then:
+        response.contentAsString == /[{"error":"o noes"},{"error":"sworded"}]/
+    }
+
 }
 
 class AlbumController {
     def index() {
         render Album.list() as GSON
     }
+
+	def error() {
+		def message = [error: 'o noes']
+		render message as GSON
+	}
+
+	def errors() {
+		def message = [[error: 'o noes'], [error: 'sworded']]
+		render message as GSON
+	}
 }
