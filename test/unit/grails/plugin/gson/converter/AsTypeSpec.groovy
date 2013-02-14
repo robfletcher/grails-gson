bfcba3b907ed24f814bdd62bbeeac15b7e39b0c8
robfletcher <robert.w.fletcher@gmail.com>
2013-02-14 12:26:28 +0000

    got test for #9 running
    still fails for Map as GSON

--- test/unit/grails/plugin/gson/converter/AsTypeSpec.groovy
+++ test/unit/grails/plugin/gson/converter/AsTypeSpec.groovy
@@ -3,29 +3,34 @@ package grails.plugin.gson.converter
 import grails.plugin.gson.GSON
 import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
-import spock.lang.Specification
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+import org.codehaus.groovy.grails.plugins.converters.ConvertersPluginSupport
+import spock.lang.*
 
+@Unroll
 @TestMixin(GrailsUnitTestMixin)
 class AsTypeSpec extends Specification {
 
-	void 'can cast a List<String> to GSON'() {
-		given:
-		def o = ['a', 'b', 'c']
-		def gson = o as GSON
-
-		expect:
-		gson instanceof GSON
-		gson[0] == 'a'
+	void setup() {
+		ConvertersPluginSupport.enhanceApplication(grailsApplication, applicationContext)
+		ApplicationHolder.application = grailsApplication
 	}
 
-	void 'can cast a List<Map> to GSON'() {
-		given:
-		def o = [[x: 'a'], [x: 'b'], [x: 'c']]
-		def gson = o as GSON
-
+	void 'can cast a #type to GSON'() {
 		expect:
-		gson instanceof GSON
-		gson[0].x == 'a'
+		toJsonString(o) == expected
+
+		where:
+		o                              | expected                          | type
+		['a', 'b', 'c']                | '["a","b","c"]'                   | 'List<String>'
+		[a: 'a', b: 'b', c: 'c']       | '{"a":"a","b":"b","c":"c"}'       | 'Map<String, String>'
+		[[x: 'a'], [x: 'b'], [x: 'c']] | '[{"x":"a"},{"x":"b"},{"x":"c"}]' | 'List<Map<String,String>>'
+	}
+
+	private String toJsonString(o) {
+		def writer = new StringWriter()
+		(o as GSON).render(writer)
+		writer.toString()
 	}
 
 }
