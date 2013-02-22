3cc70c12411c726e3cdf454858287e16492d1f5d
robfletcher <robert.w.fletcher@gmail.com>
2013-02-22 17:42:45 +0000

    use resource mapping in test project
    This breaks everything as the request inputStream gets consumed by the ParameterCreationListeners

--- test/apps/gson-test/grails-app/conf/UrlMappings.groovy
+++ test/apps/gson-test/grails-app/conf/UrlMappings.groovy
@@ -2,9 +2,8 @@ class UrlMappings {
 
 	static mappings = {
 		"/${controller}s"(action: 'list')
-		"/$controller/$id?" {
-			action = [GET: 'show', PUT: 'update', DELETE: 'delete', POST: 'save']
-		}
+		"/album/$id?"(resource: 'album')
+		"/publication/$id?"(resource: 'publication')
 
 		"404" controller: 'errors', action: 'notFound'
 		"500" controller: 'errors', action: 'serverError'
