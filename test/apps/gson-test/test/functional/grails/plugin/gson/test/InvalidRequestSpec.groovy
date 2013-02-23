13b04cd6ce47d73b9f366a5b64d685c58bf719f0
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-02-23 14:43:35 +0000

    test for errors at nested points in the graph
    fails for invalid nested id - issue #20

--- test/apps/gson-test/test/functional/grails/plugin/gson/test/InvalidRequestSpec.groovy
+++ test/apps/gson-test/test/functional/grails/plugin/gson/test/InvalidRequestSpec.groovy
@@ -1,12 +1,11 @@
 package grails.plugin.gson.test
 
 import groovyx.net.http.HttpResponseException
-import spock.lang.Unroll
-import static grails.plugin.gson.test.AlbumController.SC_UNPROCESSABLE_ENTITY
+import spock.lang.*
+import static grails.plugin.gson.http.HttpConstants.SC_UNPROCESSABLE_ENTITY
 import static groovyx.net.http.ContentType.JSON
 import static javax.servlet.http.HttpServletResponse.SC_NOT_ACCEPTABLE
 import static org.apache.http.entity.ContentType.APPLICATION_JSON
-import static grails.plugin.gson.http.HttpConstants.*
 
 @Unroll
 class InvalidRequestSpec extends RestEndpointSpec {
@@ -51,6 +50,40 @@ class InvalidRequestSpec extends RestEndpointSpec {
 		e.response.data.errors[0] == 'Property [title] of class [class grails.plugin.gson.test.Album] cannot be blank'
 	}
 
-	// TODO: should reject invalid data deep in graph
+	@Issue('https://github.com/robfletcher/grails-gson/issues/20')
+	void 'save returns a 422 if an id specified for a nested entity does not exist'() {
+		when:
+		http.post(path: 'album', body: [title: 'Hunky Dory', artist: [id: Long.MAX_VALUE]], requestContentType: JSON)
+
+		then:
+		def e = thrown(HttpResponseException)
+		e.response.status == SC_UNPROCESSABLE_ENTITY
+		e.response.data.errors[0] == 'foo'
+	}
+
+	void 'save returns a 422 with details of error for a problem at a nested level in the graph'() {
+		when:
+		http.post(path: 'album', body: [title: 'Aladdin Sane', artist: [name: null]], requestContentType: JSON)
+
+		then:
+		def e = thrown(HttpResponseException)
+		e.response.status == SC_UNPROCESSABLE_ENTITY
+		e.response.contentType == APPLICATION_JSON.mimeType
+		e.response.data.errors[0] == 'Property [name] of class [class grails.plugin.gson.test.Artist] cannot be null'
+	}
+
+	void 'update returns a 422 with details of error for a problem at a nested level in the graph'() {
+		given:
+		def album = fixtureLoader.load('albums').aThingCalledDivineFits
+
+		when:
+		http.put(path: "album/$album.id", body: [artist: [name: null]], requestContentType: JSON)
+
+		then:
+		def e = thrown(HttpResponseException)
+		e.response.status == SC_UNPROCESSABLE_ENTITY
+		e.response.contentType == APPLICATION_JSON.mimeType
+		e.response.data.errors[0] == 'Property [name] of class [class grails.plugin.gson.test.Artist] cannot be null'
+	}
 
 }
