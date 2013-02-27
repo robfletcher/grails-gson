3cc1b63598803a9454dcd2a6ce1e67769e2eb2e9
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-02-27 11:57:24 +0000

    #24 added one-to-one relationship types to test coverage

--- test/unit/grails/plugin/gson/deserialization/BidirectionalAssociationSpec.groovy
+++ test/unit/grails/plugin/gson/deserialization/BidirectionalAssociationSpec.groovy
@@ -7,7 +7,7 @@ import grails.test.mixin.Mock
 import spock.lang.*
 
 @Issue('https://github.com/robfletcher/grails-gson/issues/24')
-@Mock([Album, Artist])
+@Mock([Album, Artist, Biography, Cover])
 class BidirectionalAssociationSpec extends Specification {
 
 	Gson gson
@@ -59,11 +59,44 @@ class BidirectionalAssociationSpec extends Specification {
 		album.artist.albums == [album]
 	}
 
+	void 'bi-directional associations are populated in both directions when deserializing one-to-one'() {
+		given:
+		def data = [
+				title: 'Station to Station',
+				cover: [image: new byte[0]]
+		]
+		def json = gson.toJson(data)
+
+		when:
+		def album = gson.fromJson(json, Album)
+
+		then:
+		album.cover.album == album
+	}
+
+	void 'bi-directional associations are populated in both directions when deserializing one-to-one using hasOne'() {
+		given:
+		def data = [
+		        name: 'David Bowie',
+				bio: [text: 'David Robert Jones (born 8 January 1947), known by his' +
+						' stage name David Bowie (pron.: /ˈboʊ.i/ boh-ee),[1]' +
+						' is an English musician, actor, record producer and arranger...']
+		]
+		def json = gson.toJson(data)
+
+		when:
+		def artist = gson.fromJson(json, Artist)
+
+		then:
+		artist.bio.artist == artist
+	}
+
 }
 
 @Entity
 class Album {
 	String title
+	Cover cover
 	static belongsTo = [artist: Artist]
 }
 
@@ -71,4 +104,17 @@ class Album {
 class Artist {
 	String name
 	static hasMany = [albums: Album]
+	static hasOne = [bio: Biography]
+}
+
+@Entity
+class Biography {
+	Artist artist
+	String text
+}
+
+@Entity
+class Cover {
+	byte[] image
+	static belongsTo = [album: Album]
 }
