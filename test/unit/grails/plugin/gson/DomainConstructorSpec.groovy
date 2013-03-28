c61f143aea46e22d1f962a3f8ed30d344c889e19
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-03-28 11:20:55 +0000

    #30 test for GrailsParameterMap usage in unit tests

--- test/unit/grails/plugin/gson/DomainConstructorSpec.groovy
+++ test/unit/grails/plugin/gson/DomainConstructorSpec.groovy
@@ -5,16 +5,18 @@ import grails.converters.JSON
 import grails.plugin.gson.adapters.GrailsDomainDeserializer
 import grails.plugin.gson.api.ArtefactEnhancer
 import grails.test.mixin.*
-import grails.test.mixin.support.GrailsUnitTestMixin
+import grails.test.mixin.web.ControllerUnitTestMixin
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsParameterMap
 import spock.lang.*
 
 @Issue('https://github.com/robfletcher/grails-gson/issues/21')
-@TestMixin(GrailsUnitTestMixin)
+@TestMixin(ControllerUnitTestMixin)
 @Mock(Person)
 @Unroll
 class DomainConstructorSpec extends Specification {
 
 	@Shared def json = '{"name":"Rob"}'
+	@Shared def map = [name: 'Rob']
 
 	void setup() {
 		def gsonBuilder = new GsonBuilder()
@@ -33,10 +35,22 @@ class DomainConstructorSpec extends Specification {
 		where:
 		parameter << [
 				new JsonParser().parse(json),
-				JSON.parse(json)
+				JSON.parse(json),
+				map
 		]
 		type = parameter.getClass().name
+	}
 
+	void 'can construct a domain instance using a GrailsParameterMap'() {
+		given:
+		request.setParameter('name', 'Rob')
+		def parameter = new GrailsParameterMap(request)
+
+		when:
+		def instance = new Person(parameter)
+
+		then:
+		instance.name == 'Rob'
 	}
 
 }
